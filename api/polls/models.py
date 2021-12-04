f3140830242746dd4c274a27c20e95e78785a20f
cmabel24 <cmabel@me.com>
2021-12-04 12:33:44 -0600

    working on tests

---
+++ api/polls/models.py
@@ -0,0 +1,37 @@
+import uuid
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+class Key(models.Model):
+    id = models.UUIDField(
+        auto_created=True,
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        verbose_name="ID",
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="keys")
+
+
+class Wallet(models.Model):
+    id = models.UUIDField(
+        auto_created=True,
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        verbose_name="ID",
+    )
