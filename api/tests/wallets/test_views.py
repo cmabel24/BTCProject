1771a22944de0c21fa3ed86922446f427406eacd
cmabel24 <cmabel@me.com>
2022-01-04 16:51:36 -0500

    finshed testing working on templates

--- api/tests/wallets/test_views.py
+++ api/tests/wallets/test_views.py
@@ -3,6 +3,7 @@ from typing import Dict, Mapping
 from django.forms.fields import DateTimeField, EmailField
 from django.http.response import HttpResponseBase
 from django.test import TestCase
+from wallets.views import transaction
 
 
 class IndexTests(TestCase):
@@ -12,6 +13,9 @@ class IndexTests(TestCase):
             transaction=list,
             msg="Transaction not displayed correctly",
         )
+
+
 class DetailTests(TestCase):
     def test_transaction_detail(self):
-        self.
+        self.assertContains(HttpResponseBase, str=transaction)
+        self.failIf(expr=bool, msg="No Transaction Shown")
