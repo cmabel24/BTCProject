ea2977e17f7d1b2771eed46ecce5f8622717f5cd
cmabel24 <cmabel@me.com>
2022-03-12 15:33:01 -0500

    the errors in test are from no information

--- api/tests/wallets/test_views.py
+++ api/tests/wallets/test_views.py
@@ -1,22 +1,16 @@
+from cgitb import html
 from datetime import datetime
 from typing import Dict, Mapping
 from django.forms.fields import DateTimeField, EmailField
-from django.http import StreamingHttpResponse
+from django.http import HttpResponseNotFound, StreamingHttpResponse
 from django.http.response import HttpResponseBase
 from django.test import TestCase
 from wallets.views import transaction
 
 
-class IndexTests(TestCase):
-    def test_transaction_view(self):
-        self.assertListEqual(
-            bytes=list,
-            int=list,
-            msg="Transaction not displayed correctly",
-        )
-
-
 class DetailTests(TestCase):
     def test_transaction_detail(self):
-        self.assertContains(StreamingHttpResponse, text=bytes)
+        self.assertContains(
+            StreamingHttpResponse, text=html, count=1, status_code=HttpResponseNotFound
+        )
         self.failIf(expr=bool, msg="No Transaction Shown")
