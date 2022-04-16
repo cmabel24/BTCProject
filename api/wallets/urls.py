aa12828277b0d391ef7f6013373c234288d0418b
cmabel24 <cmabel@me.com>
2022-04-16 15:16:37 -0500

    working simple page and correct pathing

--- api/wallets/urls.py
+++ api/wallets/urls.py
@@ -1,16 +1,17 @@
 from re import template
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls import url
 from . import views
 
 app_name = "Django Bitcoin"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="BitcoinAddress"),
+    # path("", views.IndexView.as_view(), name="BitcoinAddress"),
     path("<int:pk>/wallets/", views.DetailView.as_view(), name="Wallet"),
     path("<int:pk>/transaction/", views.ResultsView.as_view(), name="Transactions"),
     path("<int:pk>/create/", views.CreateUser.as_view(), name="CreateUser"),
     path("<int:question_id>/balance/", views.ResultsView.as_view(), name="Balance"),
     path("<int:uuid>/UserHomepage", views.UserHomepage.as_view(), name="UserHomepage"),
     url(r"^(?P<slug>[a-z0-9-_]+?)/$", views.page_details, name="details"),
-    path("<int:pk>/simple/", views.Simple.as_view(), name="simple"),
+    path("", views.index, name="index"),
+    path("sections/<int:num>", views.section, name="section"),
 ]
--- api/wallets/views.py
+++ api/wallets/views.py
@@ -8,11 +8,25 @@ from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from .models import Key, User, Wallet, Transaction, Page
 import datetime
+from django.http import HttpResponse, Http404
 
 
-class Simple(generic.CreateView):
-    template_name = "Simple.html"
-    context_object_name = "Simple Create"
+def index(request):
+    return render(request, "wallets/simple.html")
+
+
+texts = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tortor mauris, maximus semper volutpat vitae, varius placerat dui. Nunc consequat dictum est, at vestibulum est hendrerit at. Mauris suscipit neque ultrices nisl interdum accumsan. Sed euismod, ligula eget tristique semper, lecleo mi nec orci. Curabitur hendrerit, est in ",
+    "Praesent euismod auctor quam, id congue tellus malesuada vitae. Ut sed lacinia quam. Sed vitae mattis metus, vel gravida ante. Praesent tincidunt nulla non sapien tincidunt, vitae semper diam faucibus. Nulla venenatis tincidunt efficitur. Integer justo nunc, egestas eget dignissim dignissim,  facilisis, dictum nunc ut, tincidunt diam.",
+    "Morbi imperdiet nunc ac quam hendrerit faucibus. Morbi viverra justo est, ut bibendum lacus vehicula at. Fusce eget risus arcu. Quisque dictum porttitor nisl, eget condimentum leo mollis sed. Proin justo nisl, lacinia id erat in, suscipit ultrices nisi. Suspendisse placerat nulla at volutpat ultricies",
+]
+
+
+def section(request, num):
+    if 1 <= num <= 3:
+        return HttpResponse(texts[num - 1])
+    else:
+        raise Http404("No such section")
 
 
 class LoginView(generic.FormView):
