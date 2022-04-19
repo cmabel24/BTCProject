a12a3fd046f3268583b4bbd0f1905e164a7ad70a
cmabel24 <cmabel@me.com>
2022-04-19 17:11:07 -0500

    starting hd wallets

--- api/wallets/views.py
+++ api/wallets/views.py
@@ -11,6 +11,11 @@ import datetime
 from django.http import HttpResponse, Http404
 from mnemonic import Mnemonic
 import secrets
+from hdwallet import HDWallet
+from hdwallet.utils import generate_entropy
+from hdwallet.symbols import BTC as SYMBOL
+from typing import Optional
+import json
 def RandInts(Size,nBits):
     VectorSize = Size
     RandomVector = [secrets.randbelow(nBits) for i in range(VectorSize)]
@@ -45,8 +50,32 @@ class CreateSeedPhrase(generic.ListView):
     mnemo = Mnemonic("english")
     words = mnemo.generate(strength=128)
     seed = mnemo.to_seed(words, passphrase="")
+    # Choose strength 128, 160, 192, 224 or 256
+    STRENGTH: int = 160  # Default is 128
+    # Choose language english, french, italian, spanish, chinese_simplified, chinese_traditional, japanese or korean
+    LANGUAGE: str = "english"  # Default is english
+    # Generate new entropy hex string
+    ENTROPY: str = generate_entropy(strength=STRENGTH)
+    # Secret passphrase for mnemonic
+    PASSPHRASE: Optional[str] = None  # "meherett"
+    # Initialize Bitcoin mainnet HDWallet
+    hdwallet: HDWallet = HDWallet(symbol=SYMBOL, use_default_path=False)
+    # Get Bitcoin HDWallet from entropy
+    hdwallet.from_entropy(
+        entropy=ENTROPY, language=LANGUAGE, passphrase=PASSPHRASE
+    )
 
+    # Derivation from path
+    # hdwallet.from_path("m/44'/0'/0'/0/0")
+    # Or derivation from index
+    hdwallet.from_index(44, hardened=True)
+    hdwallet.from_index(0, hardened=True)
+    hdwallet.from_index(0, hardened=True)
+    hdwallet.from_index(0)
+    hdwallet.from_index(0)
 
+    # Print all Bitcoin HDWallet information's
+    print(json.dumps(hdwallet.dumps(), indent=4, ensure_ascii=False))
 
 class LoginView(generic.FormView):
     template_name = "login.html"
