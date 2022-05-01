a24636a23491f4b409e334b8b98073527dc26b8f
cmabel24 <cmabel@me.com>
2022-05-01 15:56:19 -0500

    add django cc

---
+++ api/wallets/tasks/__init__.py
@@ -0,0 +1,19 @@
+from cc import (
+    logger,
+    query_transactions,
+    process_deposit_transaction,
+    query_transaction,
+    normalize_txifno,
+    refill_addresses_queue,
+    process_withdraw_transactions,
+)
+
+__all__ = [
+    logger,
+    query_transactions,
+    process_deposit_transaction,
+    query_transaction,
+    normalize_txifno,
+    refill_addresses_queue,
+    process_withdraw_transactions,
+]
