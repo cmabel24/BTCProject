5339feae14a43dc19f29c95c50ed499782e57198
cmabel24 <cmabel@me.com>
2022-05-02 17:33:53 -0500

    more getting rid of commenting

--- api/wallets/urls.py
+++ api/wallets/urls.py
@@ -8,10 +8,4 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="wallet_list"),
     path("<uuid:pk>/", views.TransactionListView.as_view(), name="wallet_detail"),
     path('create.html', views.CreateSeedPhrase.as_view(), name="CreateSeedPhrase"),
-    # path("<int:pk>/transaction/", views.ResultsView.as_view(), name="Transactions"),
-    # path("<int:question_id>/balance/", views.ResultsView.as_view(), name="Balance"),
-    # path("<int:uuid>/UserHomepage", views.UserHomepage.as_view(), name="UserHomepage"),
-    # url(r"^(?P<slug>[a-z0-9-_]+?)/$", views.page_details, name="details"),
-    # path("sections/<int:num>", views.section, name="section"),
-    
 ]
